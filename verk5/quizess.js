5232c131ac43aadc9ad472a611bfb1e92552d716
AtliOskarsson <atlio@outlook.com>
2018-02-09 10:18:15 +0000

    verk5 fyrsta viðbót

---
+++ verk5/quizess.js
@@ -0,0 +1,11 @@
+function quiz(spurning, svarmoguleikar, svar) {
+	this.spurning = spurning;
+	this.svarmoguleikar = svarmoguleikar;
+	this.svar = svar;	
+
+	spuringaStrengur = "<div>" + spurning + "<div>";
+	for (x in svarmoguleikar) {
+
+	}
+	document.getElementById("quiz").innerHTML = "3123123";
+};
