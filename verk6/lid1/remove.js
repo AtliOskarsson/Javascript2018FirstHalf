082b8af4fdaac9e7be67591deefc76089b90d34a
AtliOskarsson <atlio@outlook.com>
2018-02-18 23:04:34 +0000

    Update remove.js

--- verk6/lid1/remove.js
+++ verk6/lid1/remove.js
@@ -1,1 +1,3 @@
-let main = document.getElementByTagName('main');
+document.getElementsByTagName("div")[0].setAttribute("class", "active");
+
+document.getElementsByTagName("div")[2].parentNode.removeChild(karen);
